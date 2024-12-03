ce4eb539894b3c93e4a7e06b18a84c6b918292c8
Cris Alvariño <calvarin@uji.es>
2024-12-03 14:22:54 +0100

    Añadido el API para consultar el código

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ const codigosValidos = ["69abd4abf577d7cfd6d370f146611fea", "XYZ789", "123456"];
 
 function onScanSuccess(decodedText) {
   const divEstado = document.getElementById("status");
-  if (codigosValidos.includes(decodedText)) {
-    divEstado.textContent = `Acceso permitido: ${decodedText}`;
-    divEstado.className = "valid";
-  } else {
-    divEstado.textContent = `Acceso denegado: ${decodedText}`;
-    divEstado.className = "invalid";
-  }
+  const apiUrl = `https://674ef8f4bb559617b26d7b3b.mockapi.io/accesos/codigos/${decodedText}`;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (response.status === 404) {
+        throw new Error('Not found');
+      }
+      return response.json();
+    })
+    .then(data => {
+      divEstado.textContent = `Acceso permitido: ${decodedText}`;
+      divEstado.className = "valid";
+    })
+    .catch(error => {
+      if (error.message === 'Not found') {
+        divEstado.textContent = `Acceso denegado: ${decodedText} no encontrado.`;
+      } else {
+        divEstado.textContent = `Error al validar el código: ${decodedText}`;
+      }
+      divEstado.className = "invalid";
+      console.warn(`Error al validar el código: ${error}`);
+    });
 }
 
 function onScanFailure(error) {
